714c19598ab5b44af1955534ba16d9ef2fc45237
Renato Ikeda Bressan <renatoikeda98@gmail.com>
2025-11-10 08:24:06 -0300

    Add files via upload

---
+++ C/strchr.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+char *str_chr(const char *str, int ch);
+
+void conta_ocorrencias(const char *str, char ch, int *occurrences);
+
+int main() {
+    char *str, ch;
+    int count;
+    str = (char *)malloc(strlen(str) * sizeof(char));
+    scanf("%[^\n]%*c %c", str, &ch);
+    if (str_chr(str, ch) == NULL) {
+        printf("'%c' nao pertence a '%s', portanto, nao foi possivel contar as ocorrencias.\n", ch, str);
+    } else {
+        conta_ocorrencias(str, ch, &count);
+        printf("'%c' aparece %d vezes em '%s'.\n", ch, count, str);
+    }
+    free(str);
+    return 0;
+}
+
+char *str_chr(const char *str, int ch) {
+    while (*str != '\0') {
+        if (*str == ch) return (char *)str;
+        str++;
+    }
+    if (ch == '\0') return (char *)str;
+    return NULL;
+}
+
+void conta_ocorrencias(const char *str, char ch, int *occurrences) {
+    int count = 0;
+    const char *ptr = str;
+    while ((ptr = str_chr(ptr, ch)) != NULL) {
+        count++;
+        ptr++;
+    }
+    *occurrences = count;
+}
